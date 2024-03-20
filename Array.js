70057b64d5169c14db8409ecf60cef1b7b8a4a4e
sumitgujral <smtgjrl@gmail.com>
2024-03-20 23:53:09 +0530

    new array with function created

---
+++ Array.js
@@ -0,0 +1,8 @@
+var ourArray = ["This ", "Code ", 4 ,"Array ", "Items" ];
+function ourFunction(){
+for(var pointer=0;pointer<ourArray.length;pointer++){
+console.log(ourArray[pointer]);
+}
+}
+
+ourFunction(); 
